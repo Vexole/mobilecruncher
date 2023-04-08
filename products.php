1b0bde04131a0235dae0ae4efa32ffc19ae56b7d
Malena Aguiar <malenacaroline01@gmail.com>
2023-04-08 18:35:48 -0400

    add checkout style, compress images, change menu option position, and fix some design issues

--- products.php
+++ products.php
@@ -32,19 +32,19 @@ require_once("./db/Product.php");
             <li class="nav-item">
                 <a class="nav-link mc-nav-link" href="my_accounts.php">My Accounts</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link mc-nav-link" href="personal_support.php">Customer Support</a>
-            </li>
             <li class="nav-item dropdown">
                 <a class="nav-link mc-nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Login/Register
                 </a>
                 <ul class="dropdown-menu">
-                <li><a class="dropdown-item mc-color-gray-02" href="login.php"><i class="bi bi-box-arrow-in-right me-2"></i>Login</a></li>
-                <li><a class="dropdown-item mc-color-gray-02" href="change_password.php"><i class="bi bi-person-add me-2"></i>Change Password</a></li>
-                <li><a class="dropdown-item mc-color-gray-02" href="register.php"><i class="bi bi-person-add me-2"></i>Register</a></li>
-                <li><a class="dropdown-item mc-color-gray-02" href="logout.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
+                    <li><a class="dropdown-item mc-color-gray-02" href="login.php"><i class="bi bi-box-arrow-in-right me-2"></i>Login</a></li>
+                    <li><a class="dropdown-item mc-color-gray-02" href="change_password.php"><i class="bi bi-person-add me-2"></i>Change Password</a></li>
+                    <li><a class="dropdown-item mc-color-gray-02" href="register.php"><i class="bi bi-person-add me-2"></i>Register</a></li>
+                    <li><a class="dropdown-item mc-color-gray-02" href="logout.php"><i class="bi bi-box-arrow-right me-2"></i>Logout</a></li>
                 </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mc-nav-link" href="personal_support.php">Customer Support</a>
             </li>
             </ul>
         </div>
